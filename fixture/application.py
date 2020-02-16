66207e455002c4c567ff82847b658f1acf4667b9
simaoua <lukianetsandrew@gmail.com>
2020-02-16 18:48:40 +0200

    Add files via upload

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,14 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-wd = WebDriver
 from fixture.session import SessionHelper
 
-class Application:
+class Application(WebDriver):
 
     def __init__(self):
-        self.wd = Webdriver()
+        self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
 
-    def test_open_homepage(self):
+    def open_homepage(self):
         wd = self.wd
         wd.get("https://account.usafootball.com/login/")
 
--- fixture/session.py
+++ fixture/session.py
@@ -4,17 +4,17 @@ class SessionHelper:
     def __init__(self, app):
         self.app = app
 
-    def test_login(self, username, password):
+    def login(self, username, password):
         wd = self.app.wd
         self.app.open_homepage()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_id("password").click()
+        wd.find_element_by_id("password").clear()
+        wd.find_element_by_id("password").send_keys(password)
         wd.find_element_by_css_selector('div.logBox button').click()
 
-    def test_logout(self):
+    def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Log Out").click()
